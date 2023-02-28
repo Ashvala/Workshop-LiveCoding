594625bbd02289db90951e8343d21bf6f0923498
Ian Clester <ijc@ijc8.me>
2023-02-28 18:01:06 -0500

    Placeholders for remaining slides

--- src/Presentation.svelte
+++ src/Presentation.svelte
@@ -1,19 +1,27 @@
 <script>
-    import Title          from './slides/Title.svelte';
-    import WhatIsLiveCoding from './slides/WhatIsLiveCoding.svelte';
-    import IncompleteList from './slides/IncompleteList.svelte';
-    // import AutoAnimate from './slides/examples/AutoAnimate.svelte';
-    // import Backgrounds from './slides/examples/Backgrounds.svelte';
-    // import Barebones from './slides/examples/Barebones.svelte';
-    import LayoutHelpers from './slides/examples/LayoutHelpers.svelte';
-    // import Markdown from './slides/examples/Markdown.svelte';
-    // import Math from './slides/examples/Math.svelte';
-    // import Media from './slides/examples/Media.svelte';
-    // import Transitions from './slides/examples/Transitions.svelte';
-    import WhatArePatterns from "./slides/WhatArePatterns.svelte";
-    import LiveCodingExamples from './slides/LiveCodingExamples.svelte';
-    import WhatIsStrudel from './slides/WhatIsStrudel.svelte';
-    const partner = ['Svelte', 'Reveal.js'];
+    import Title from './slides/Title.svelte'
+    import WhatIsLiveCoding from './slides/WhatIsLiveCoding.svelte'
+    import IncompleteList from './slides/IncompleteList.svelte'
+    // import AutoAnimate from './slides/examples/AutoAnimate.svelte'
+    // import Backgrounds from './slides/examples/Backgrounds.svelte'
+    // import Barebones from './slides/examples/Barebones.svelte'
+    import LayoutHelpers from './slides/examples/LayoutHelpers.svelte'
+    // import Markdown from './slides/examples/Markdown.svelte'
+    // import Math from './slides/examples/Math.svelte'
+    // import Media from './slides/examples/Media.svelte'
+    // import Transitions from './slides/examples/Transitions.svelte'
+    import WhatArePatterns from "./slides/WhatArePatterns.svelte"
+    import LiveCodingExamples from './slides/LiveCodingExamples.svelte'
+    import WhatIsStrudel from './slides/WhatIsStrudel.svelte'
+    import MiniNotation from './slides/MiniNotation.svelte'
+    import PatternConstructors from './slides/PatternConstructors.svelte'
+    import Modifiers from './slides/Modifiers.svelte'
+    import TipsAndTricks from './slides/TipsAndTricks.svelte'
+    import CollaborativeLiveCoding from './slides/CollaborativeLiveCoding.svelte'
+    import LiveCodingVisuals from './slides/LiveCodingVisuals.svelte'
+    import ReferencesAndResources from './slides/ReferencesAndResources.svelte'
+    import Thanks from './slides/Thanks.svelte'
+    const partner = ['Svelte', 'Reveal.js']
 </script>
 
 <Title/>
@@ -23,3 +31,13 @@
 <LiveCodingExamples />
 <WhatIsStrudel />
 <WhatArePatterns />
+<MiniNotation />
+<PatternConstructors />
+<MiniNotation />
+<PatternConstructors />
+<Modifiers />
+<TipsAndTricks />
+<CollaborativeLiveCoding />
+<LiveCodingVisuals />
+<ReferencesAndResources />
+<Thanks />
---
+++ src/slides/Modifiers.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Slide from '../lib/Slide.svelte'
+    import Strudel from '../lib/Strudel.svelte'
+</script>
+
+<Slide>
+    TODO
+</Slide>
