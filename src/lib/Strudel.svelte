aa6092c11a1507606849180774f0728f56c021db
Ian Clester <ijc@ijc8.me>
2023-02-28 17:11:00 -0500

    Simplify Strudel component to avoid syntax issues

--- src/lib/Strudel.svelte
+++ src/lib/Strudel.svelte
@@ -1,8 +1,5 @@
 <!-- Based on https://www.npmjs.com/package/@strudel.cycles/embed?activeTab=explore -->
 <script>
-    let data
+    export let code
 </script>
-<span bind:this={data} style="display: none"><slot /></span>
-{#if data}
-    <iframe width={600} height={400} src={`https://strudel.tidalcycles.org/#${encodeURIComponent(btoa(data?.innerHTML))}`} title="Strudel"></iframe>
-{/if}
+<iframe width={600} height={400} src={`https://strudel.tidalcycles.org/#${encodeURIComponent(btoa(code))}`} title="Strudel"></iframe>
